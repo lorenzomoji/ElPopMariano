15a3235c1b1077bc136123bbef626f6f3c396abe
unknown <mojima16@gmail.com>
2019-12-08 18:20:32 +0100

    Actualizar

--- llamadaPHP.js
+++ llamadaPHP.js
@@ -1,21 +1,27 @@
 export var diario;
+import { DIARIO } from 'diario.js';
 function AJAX() {
     const http = new XMLHttpRequest();
     const url = 'back/conexionBBDD.php';
     
-    http.open("GET", url);
-    http.onreadystatechange = function recogerDiario() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("response").innerHTML = this.responseText;
-            console.log('Response: ', this.responseText);
-            // diario.push(this.responseText);
-            diario = JSON.parse(this.responseText);
-            console.log(diario);
+    try {
+        http.open("GET", url);
+        http.onreadystatechange = function recogerDiario() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("response").innerHTML = this.responseText;
+                console.log('Response: ', this.responseText);
+                // diario.push(this.responseText);
+                diario = JSON.parse(this.responseText);
+                console.log(diario);
+            }
+            return diario;
         }
-        return diario;
-    }
-    http.send();
-}
+        http.send();
+        } catch(error) {
+            console.error(error);
+            diario = DIARIO;
+        } //En caso de que no se recoja del back, que recoja el diario de un JSON.
+    } 
 
 AJAX();
 
